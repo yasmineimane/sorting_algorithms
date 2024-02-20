220cee18fb5843fc30493d510e4ac59eadb281a9
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-02-20 07:43:22 -0800

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -10,17 +10,26 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, ind_min, tmp;
 
+	if (!array || size == 1)
+		return;
+
 	for (i = 0 ; i < size - 1 ; i++)
 	{
 		ind_min = i;
 		for (j = i + 1 ; j < size ; j++)
 		{
 			if (array[ind_min] > array[j])
+			{
 				ind_min = j;
+				continue;
+			}
 		}
-		tmp = array[i];
-		array[i] = array[ind_min];
-		array[ind_min] = tmp;
-		print_array(array, size);
+		if (ind_min != i)
+		{
+			tmp = array[i];
+			array[i] = array[ind_min];
+			array[ind_min] = tmp;
+			print_array(array, size);
+		}
 	}
 }
