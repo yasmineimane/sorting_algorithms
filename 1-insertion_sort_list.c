ed9f35b9bc26a4bd00d734ca069a421f57c4d37e
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-02-21 13:11:04 -0800

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers in ascending order.
+ * insertion_sort_list - sorts a dll of integers in ascending order.
  * @list: the list to be sorted.
  */
 
